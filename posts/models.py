a70b55b06a2710702283224007ea92c0d2bd90d8
João Pedro Beker <joaopedro.beker@hotmail.com>
2022-07-18 15:20:23 -0300

    resizing post_images

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,9 @@ from django import forms
 from django.utils import timezone
 from django.contrib.auth.models import User
 from categories.models import Category
+from PIL import Image
+from django.conf import settings
+import os
 
 
 class Post(models.Model):
@@ -17,6 +20,29 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.resize_image(self.image, 800)
+
+    @staticmethod
+    def resize_image(image_name, new_image_width):
+        image_path = os.path.join(settings.MEDIA_ROOT, str(image_name))
+        image = Image.open(image_path)
+        image_width, image_height = image.size
+
+        if image_width <= new_image_width:
+            image.close()
+        else:
+            new_image_height = int((new_image_width * image_height) / image_width)
+            new_image = image.resize((new_image_width, new_image_height), Image.ANTIALIAS)
+            new_image.save(
+                image_path,
+                optimize=True,
+                quality=60
+            )
+            new_image.close()
+            image.close()
 
 
 class PostForm(forms.ModelForm):
