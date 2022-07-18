ff86df94a2367ddb8aefa02cf2f058664b4b8238
João Pedro Beker <joaopedro.beker@hotmail.com>
2022-07-18 16:43:35 -0300

    recaptcha is now requested when user posts a new comment

--- comments/models.py
+++ comments/models.py
@@ -3,6 +3,7 @@ from django import forms
 from django.contrib.auth.models import User
 from posts.models import Post
 from django.utils import timezone
+import requests
 
 
 class Comment(models.Model):
@@ -17,21 +18,45 @@ class Comment(models.Model):
 
 class CommentForm(forms.ModelForm):
     def clean(self):
-        data = self.cleaned_data
-        name = data.get('name')
-        comment = data.get('comment')
+        raw_data = self.data
+        recaptcha_client_response = self.data.get('g-recaptcha-response')
 
-        if len(name) < 4:
-            self.add_error(
-                'name',
-                'Error: Name needs 5 characters at least!'
-            )
-
-        if len(comment) < 10:
+        if not recaptcha_client_response:
             self.add_error(
                 'comment',
-                'Error: Comment needs 10 characters at least!'
+                'Error: Recaptcha is needed!'
             )
+        else:
+
+            recaptcha_server_response = requests.post(
+                'https://www.google.com/recaptcha/api/siteverify',
+                data={
+                    'secret': '6LfN3P4gAAAAAFajeQA6w0a8WHhkxpazyzWRbU_J',
+                    'response': recaptcha_client_response
+                }
+            ).json()
+
+            if not recaptcha_server_response.get('success'):
+                self.add_error(
+                    'comment',
+                    'Error: Invalid Recaptcha'
+                )
+            else:
+                cleaned_data = self.cleaned_data
+                name = cleaned_data.get('name')
+                comment = cleaned_data.get('comment')
+
+                if len(name) < 4:
+                    self.add_error(
+                        'name',
+                        'Error: Name needs 5 characters at least!'
+                    )
+
+                if len(comment) < 10:
+                    self.add_error(
+                        'comment',
+                        'Error: Comment needs 10 characters at least!'
+                    )
 
     class Meta:
         model = Comment
