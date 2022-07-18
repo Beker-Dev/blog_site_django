a3786fa31b56aa7b6c5ff5be80a86503b6965979
João Pedro Beker <joaopedro.beker@hotmail.com>
2022-07-18 14:45:02 -0300

    changed UpdateView to View class in post_detail

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
+from django.views import View
 from django.db.models import Q, Case, Count, When
 from .models import Post
 from comments.models import Comment, CommentForm
@@ -65,29 +66,62 @@ class PostCategory(PostIndex):
             return qs
 
 
-class PostDetail(UpdateView):
-    model = Post
+class PostDetail(View):
     template_name = 'posts/post_detail.html'
-    context_object_name = 'post'
-    form_class = CommentForm
+    context = {}
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        post = self.get_object()
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        pk = self.kwargs.get('pk')
+        post = get_object_or_404(Post, id=pk, is_published=True)
         comments = Comment.objects.filter(is_published=True, post=post.id)
-        context['comments'] = comments
+        form = CommentForm(request.POST or None)
+        self.context = {
+            'post': post,
+            'comments': comments,
+            'form': form
+        }
 
-        return context
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.context)
 
-    def form_valid(self, form):
-        post = self.get_object()
-        comment = Comment(**form.cleaned_data)
-        comment.post = post
+    def post(self, request, *args, **kwargs):
+        form = self.context.get('form')
+        if form is not None and form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user if request.user.is_authenticated else None
+            comment.post = self.context.get('post')
+            comment.save()
+            messages.success(request, 'Comment sent successfully!')
 
-        if self.request.user.is_authenticated:
-            comment.user = self.request.user
+            return redirect('post_detail', pk=comment.post.id)
+        else:
+            return render(request, self.template_name, self.context)
 
-        comment.save()
-        messages.success(self.request, 'Comment sent successfully!')
-        return redirect('post_detail', pk=post.id)
+
+# class PostDetail(UpdateView):
+#     model = Post
+#     template_name = 'posts/post_detail.html'
+#     context_object_name = 'post'
+#     form_class = CommentForm
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         post = self.get_object()
+#         comments = Comment.objects.filter(is_published=True, post=post.id)
+#         context['comments'] = comments
+#
+#         return context
+#
+#     def form_valid(self, form):
+#         post = self.get_object()
+#         comment = Comment(**form.cleaned_data)
+#         comment.post = post
+#
+#         if self.request.user.is_authenticated:
+#             comment.user = self.request.user
+#
+#         comment.save()
+#         messages.success(self.request, 'Comment sent successfully!')
+#         return redirect('post_detail', pk=post.id)
 
